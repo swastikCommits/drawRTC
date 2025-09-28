69212a6f7dbd57940833cc991f8c105a17c712d0
swastikCommits <textswastik.alt@gmail.com>
2025-09-28 06:47:03 +0530

    monorepo init plus ws & http boilerplate

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,11 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        ws.send("Welcome to the WebSocket server");
+    });
+    
+});
+
