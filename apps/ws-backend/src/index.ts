985f54954b47a26b24f97e7ad47de107c7e1e7cf
swastikCommits <textswastik.alt@gmail.com>
2025-09-28 11:41:14 +0530

    token authorization as queryparam in ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
+    const url = request.url;
+    if(!url) return;
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || !decoded.userId){
+        ws.close();
+        return;
+    }
+
+    
     ws.on("message", (message) => {
-        ws.send("Welcome to the WebSocket server");
+        ws.send("Pong");
     });
     
 });
