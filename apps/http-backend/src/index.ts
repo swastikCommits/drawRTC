e43525fa01d65ab7b82f867905085d4db492df27
swastikCommits <textswastik.alt@gmail.com>
2025-09-28 08:55:13 +0530

    added http endpoints and middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,29 @@
 import express from "express";
+import { middleware } from "./middleware";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const app = express();
+
+app.post("/signup", (req, res) => {
+
+    res.json({
+        userId: 1
+    })
+});
+
+app.post("/signin", (req, res) => {
+    const userId = 1;
+    const token = jwt.sign({userId}, JWT_SECRET);
+    res.json({ token });
+});
+app.post("/create", middleware, (req, res) => {
+
+    res.json({
+        roomId: 123
+    })
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+
+
+export const middleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(decoded) {
+        (req as any).userId = decoded.userId;
+        next();
+    } else {
+        res.status(403).json({ message: "Unauthorized" });
+    }
+
+}
