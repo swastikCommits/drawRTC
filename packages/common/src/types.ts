b19dd2bf9390f8101cec0d4452c92d485c1023f2
swastikCommits <textswastik.alt@gmail.com>
2025-10-01 14:21:41 +0530

    signin endpoint expects unique email

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -7,7 +7,7 @@ export const CreateUserSchema = z.object({
 });
 
 export const SignInSchema = z.object({
-    username: z.string().min(3),
+    email: z.string().email(),
     password: z.string()
 });
 
