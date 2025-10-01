4b60ba9b45f72589984f9170af485bf91d992f78
swastikCommits <textswastik.alt@gmail.com>
2025-10-01 16:38:32 +0530

    token verification for ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -3,6 +3,13 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
+function checkUser(token:string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || !(decoded as JwtPayload).userId){
+        return null;
+    }
+    return (decoded as JwtPayload).userId;
+}
 
 wss.on("connection", (ws, request) => {
     const url = request.url;
@@ -11,10 +18,9 @@ wss.on("connection", (ws, request) => {
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get("token") || "";
 
-    const decoded = jwt.verify(token, JWT_SECRET);
-    if(!decoded || !(decoded as JwtPayload).userId){
+    const userId = checkUser(token);
+    if(!userId){
         ws.close();
-        return;
     }
 
     
